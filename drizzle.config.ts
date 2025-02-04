aa996ba274d7d46043fd5167395e4019863db9aa
Vijeta Priya <67923889+VijetaPriya47@users.noreply.github.com>
2025-02-04 20:15:54 +0530

    Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
+
 dotenv.config({ path: ".env" });
 
 export default {
-  driver: "pg",
+  dialect: "postgresql",
   schema: "./src/lib/db/schema.ts",
+  out: "./drizzle", // Adjust this output directory
+
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url : process.env.DATABASE_URL!,
   },
 } satisfies Config;
 
 // npx drizzle-kit push:pg
+
